14ec053c15d508b5439cdbc725dd39cfa21e11db
Amar Rajput <amarrajput63@gmail.com>
2021-06-22 16:49:49 +0100

    added a very basic backend

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request
 
-#import pandas as pd
+import os
+import sqlite3 as sql
+import pandas as pd
 #import numpy as np
 #import random
 #import datetime
+import json
+import plotly
 
+DATABASE = os.path.abspath(os.path.dirname(__file__)) + "/test_db.db"
+RAW_DATA_TABLE = 'test_data_table'
 
 app = Flask(__name__)
 
@@ -17,8 +23,25 @@ def index():
 def login_page():
     return render_template("test.html")
 
-
-
+@app.route("/_test_query", methods=['GET', 'POST'])
+def get_test_row():
+    test_rows = []
+    with sql.connect(DATABASE) as connection:
+        try:
+            cur = connection.cursor()
+            cur.execute("SELECT * FROM {}".format(RAW_DATA_TABLE))
+            rows = cur.fetchall()
+            df = pd.DataFrame(rows)
+            df.columns = [tuple[0] for tuple in cur.description]
+            for index, row in df.iterrows():
+                test_rows.append(row['test_string'] + ", " + str(row['test_int']))
+        except Exception as e:
+            connection.rollback()
+            print("error querying room numbers from database...Exception: " + str(e))
+    update_dict = dict(test_results = test_rows, response_string = "communication OK")
+    update = json.dumps(update_dict, cls=plotly.utils.PlotlyJSONEncoder)
+    print(test_rows)
+    return update
 
 
 if __name__ == "__main__":
--- static/js/client.js
+++ static/js/client.js
@@ -4,8 +4,8 @@ async function test_clicked() {
     const res = await fetch('/_test_query', {method: "GET",});
     const update = await res.json();
     if (res.ok && res.status == 200){
-        console.log(update.response_string);
-        output.innerText = update.test_result;
+        console.log(update);
+        output.innerText = update.test_results;
     } else { 
         console.log("Error retrieving test row from database...") 
     }
