deb778e4e8f610f5d6ec3721365d3d44c2c785ba
Amar Rajput <amarrajput63@gmail.com>
2021-06-22 16:53:44 +0100

    added some comments for clarity

--- app.py
+++ app.py
@@ -28,20 +28,19 @@ def get_test_row():
     test_rows = []
     with sql.connect(DATABASE) as connection:
         try:
-            cur = connection.cursor()
-            cur.execute("SELECT * FROM {}".format(RAW_DATA_TABLE))
-            rows = cur.fetchall()
-            df = pd.DataFrame(rows)
-            df.columns = [tuple[0] for tuple in cur.description]
-            for index, row in df.iterrows():
+            cur = connection.cursor() # create a cursor to write SQL commands to database
+            cur.execute("SELECT * FROM {}".format(RAW_DATA_TABLE))  # execute command on the relevant table (* means all)
+            rows = cur.fetchall() # grab all the resulting rows from the above query
+            df = pd.DataFrame(rows) # store the rows into a pandas dataframe
+            df.columns = [tuple[0] for tuple in cur.description] # grab the headers from the sql columns and assign the pandas df headers accordingly
+            for index, row in df.iterrows(): # add each row in the dataframe to the list that gets sent back to the client
                 test_rows.append(row['test_string'] + ", " + str(row['test_int']))
         except Exception as e:
             connection.rollback()
             print("error querying room numbers from database...Exception: " + str(e))
-    update_dict = dict(test_results = test_rows, response_string = "communication OK")
-    update = json.dumps(update_dict, cls=plotly.utils.PlotlyJSONEncoder)
-    print(test_rows)
-    return update
+    update_dict = dict(test_results = test_rows, response_string = "communication OK") # create a dict of variables to send back to client
+    update = json.dumps(update_dict, cls=plotly.utils.PlotlyJSONEncoder) # here's an example of a plotly encoder as well as a json dump
+    return update # send response back to client in JSON format
 
 
 if __name__ == "__main__":
--- static/js/client.js
+++ static/js/client.js
@@ -1,11 +1,11 @@
 var output = document.getElementById("test_output");
 
 async function test_clicked() {
-    const res = await fetch('/_test_query', {method: "GET",});
-    const update = await res.json();
+    const res = await fetch('/_test_query', {method: "GET",}); // create a fetch request to the server
+    const update = await res.json(); // store the response in a constant
     if (res.ok && res.status == 200){
-        console.log(update);
-        output.innerText = update.test_results;
+        console.log(update); // log the response to browser console
+        output.innerText = update.test_results; //update the text to show the results
     } else { 
         console.log("Error retrieving test row from database...") 
     }
